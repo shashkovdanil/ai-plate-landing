95fea89f83cd6a28418cb7e0fe6950cc422d0772
Sukairo-02 <sreka9056@gmail.com>
2024-04-21 23:58:45 +0300

    Fixed schema exports for drizzle-graphql to work properly

--- src/services/db/schemas/index.ts
+++ src/services/db/schemas/index.ts
@@ -1,5 +1,8 @@
-export { users } from "./users";
 export { plates } from "./plates";
+export { platesToPresets } from './plates-to-presets';
 export { presets } from "./presets";
+export { recipes } from './recipes';
+export { users } from "./users";
 
 export * from "./relations";
+
